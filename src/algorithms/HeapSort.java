e8c2e9d03fd3fd786b400d49900faa3b0da38993
Thomas Binu <thomas.binu@gistec.com>
2016-02-22 15:25:04 +0400

    Heap sort completed

--- src/algorithms/HeapSort.java
+++ src/algorithms/HeapSort.java
@@ -8,6 +8,7 @@ import java.util.Arrays;
  * Created by tmsbn on 2/22/16.
  */
 public class HeapSort implements AlgorithmsBase {
+
     @Override
     public void execute() {
 
@@ -18,28 +19,40 @@ public class HeapSort implements AlgorithmsBase {
         int lastInternalNode = (last - 1) / 2;
 
         for (int i = lastInternalNode; i >= 0; i--) {
-            maxHeapify(numbers, i);
+            maxHeapify(numbers, i, numbers.length);
         }
+        sort(numbers);
 
         System.out.println("The heapified array is " + Arrays.toString(numbers));
 
     }
 
-    private void maxHeapify(int[] array, int currentIndex) {
+    private void maxHeapify(int[] array, int currentIndex, int heapSize) {
 
 
         int largest = currentIndex;
         int left = currentIndex * 2 + 1;
         int right = currentIndex * 2 + 2;
 
-        if (left < array.length && array[left] > array[currentIndex])
+        if (left < heapSize && array[left] > array[currentIndex])
             largest = left;
-         if (right < array.length && array[right] > array[largest])
+        if (right < heapSize && array[right] > array[largest])
             largest = right;
 
         if (largest != currentIndex) {
             swap(currentIndex, largest, array);
-            maxHeapify(array, largest);
+            maxHeapify(array, largest, heapSize);
+        }
+
+    }
+
+    private void sort(int[] heapifiedArray) {
+
+        int length = heapifiedArray.length;
+
+        for (int i = length - 1; i > 0; i--) {
+            swap(0, i, heapifiedArray);
+            maxHeapify(heapifiedArray, 0, i - 1);
         }
 
     }
